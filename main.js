17b1029c8bb50ab17b1b6b8d2369cc0c1448e247
Alex Matthews <31320460+amatthews4851@users.noreply.github.com>
2018-03-25 19:22:19 -0500

    Updated main.js
    Updated main.js to get rid of code and imports no longer needed.

--- main.js
+++ main.js
@@ -4,11 +4,9 @@ const express = require('express'),
 	app = express();
 
 let bodyParser = require('body-parser');
-const data = require('./src/values.json');
 const formidable = require('formidable');
 const path = require('path');
 const fs = require('fs');
-const getpixels = require('get-pixels');
 const spawn = require('child_process').spawn;
 
 app.use(bodyParser.json());
@@ -62,47 +60,6 @@ app.post('/getmatch', function (req, res) {
 	// once its finished saving, do the business. This is where the magic comparing happens
 	form.on('end', () => {
 		runJoshsPython(filepath, res);
-		// setTimeout(() => {
-		// 	// get pixels in the form pixelsArray[x][y] = [r, g, b, a]
-		// 	let pixelsArray = getArrayPixels(filepath);
-
-		// 	// pause for a 1/20th of a second for the function above to work
-		// 	setTimeout(() => {
-		// 		// alrighty boys. This is where the search function is going to be.
-
-		// 		// arrays for algorithm
-		// 		let compare = [];
-		// 		let differences = [];
-
-		// 		// height array for user uploaded file
-		// 		let original = getHeightGraph(pixelsArray);
-
-		// 		// load arrays from JSON
-		// 		for (let element in data) {
-		// 			compare.push(getHeightGraph(data[element]));
-		// 		}
-
-		// 		compare.forEach(element => {
-		// 			let sum = 0;
-		// 			for (let x = 0; x < element.length; x++) {
-		// 				sum += Math.abs(original[x] - element[x]);
-		// 			}
-		// 			differences.push(sum);
-		// 		});
-
-		// 		// create object with key=height, value=name pairs to find winners
-		// 		let names = Object.getOwnPropertyNames(data);
-		// 		let winners = {};
-		// 		let counter = 0;
-		// 		differences.forEach(difference => {
-		// 			winners[difference] = names[counter];
-		// 			counter++;
-		// 		});
-
-		// 		names = Object.getOwnPropertyNames(winners);
-
-		// 	}, 50);
-		// }, 50);
 	});
 });
 
@@ -110,57 +67,6 @@ app.post('/getmatch', function (req, res) {
 const server = app.listen(3000, function () {
 	console.log(`Server started on port ${server.address().port}!`);
 });
-
-// this function gets the pixels of the image of the specified path
-let getArrayPixels = function (imagepath) {
-
-	// set up arrays
-	let otherData = [],
-		yarray = [],
-		pixel = [];
-
-	// get pixels in raw form
-	getpixels(imagepath, function (err, pixels) {
-
-		// convaluted way to get pixels in the easy form
-		for (let x = 0; x < 1024; x++) {
-			for (let y = 0; y < 768; y++) {
-				for (let z = 0; z < 4; z++) {
-					pixel.push(pixels.get(x, y, z));
-				}
-				yarray.push(pixel);
-				pixel = [];
-			}
-			otherData.push(yarray);
-			yarray = [];
-		}
-	});
-	return otherData;
-};
-
-let getHeightGraph = function (imageArray) {
-	let heightGraph = [];
-
-	for (let x = 155; x < 850; x++) {
-		let black = false;
-		let y = 724;
-
-		while (!black) {
-			if (y < 0) {
-				y = 724;
-				black = true;
-				heightGraph.push(y);
-			} else if (imageArray[x][y][0] < 25) {
-				black = true;
-				heightGraph.push(y);
-			} else {
-				y--;
-			}
-		}
-	}
-
-	return heightGraph;
-};
 
 let runJoshsPython = function (pathToPDF, res) {
 	const pythonFile = spawn('python', ['Pull Data From Pdf.py', pathToPDF]);
