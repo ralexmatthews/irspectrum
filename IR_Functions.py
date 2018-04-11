e70fff5605db36e246d8b9c00ac3194ca6f0c839
AaronKruger <krugera994@gmail.com>
2018-04-11 13:03:53 -0500

    Add files via upload
    Allows for same functionality as before, with the addition of PullText and PullStructure functions, as well as cleaner code

--- IR_Functions.py
+++ IR_Functions.py
@@ -7,67 +7,118 @@ from os import path
 
 warnings.filterwarnings("ignore")
 
-number = 0
-
 def PullImages(filename):
 
     '''
-    Pull graph images from PDF's
+    Pull graph image from first page of PDF
     '''
+
+    file = PyPDF2.PdfFileReader(open(filename, "rb"))
+    xObject = file.getPage(0)
+
+
+    xObject = xObject['/Resources']['/XObject'].getObject()
+
+    images=[]
     
-    def recurse(page, xObject):
-        global number
+    for obj in xObject:
 
-        xObject = xObject['/Resources']['/XObject'].getObject()
-
-        images=[]
-        
-        for obj in xObject:
-
-            if xObject[obj]['/Subtype'] == '/Image':
-                size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
-                data = xObject[obj]._data
-                if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
-                    mode = "RGB"
-                else:
-                    mode = "P"
-
-                #imagename = "%s"%(abspath[:-4], p, obj[1:])
-
-                if xObject[obj]['/Filter'] == '/FlateDecode':
-                    img = Image.frombytes(mode, size, data)
-                    img.save(filename + ".png")
-                    number += 1
-                    images+=[filename + ".png"]
-                elif xObject[obj]['/Filter'] == '/DCTDecode':
-                    img = open(filename + ".jpg", "wb")
-                    img.write(data)
-                    img.close()
-                    number += 1
-                    images+=[filename + ".jpg"]
-                elif xObject[obj]['/Filter'] == '/JPXDecode':
-                    img = open(filename + ".jp2", "wb")
-                    img.write(data)
-                    img.close()
-                    number += 1
-                    images+=[filename + ".jp2"]
+        if xObject[obj]['/Subtype'] == '/Image':
+            size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
+            data = xObject[obj]._data
+            if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
+                mode = "RGB"
             else:
-                recurse(page, xObject[obj])
-        return images
+                mode = "P"
 
+            if xObject[obj]['/Filter'] == '/FlateDecode':
+                img = Image.frombytes(mode, size, data)
+                img.save(filename + ".png")
+                images+=[filename + ".png"]
+            elif xObject[obj]['/Filter'] == '/DCTDecode':
+                img = open(filename + ".jpg", "wb")
+                img.write(data)
+                img.close()
+                images+=[filename + ".jpg"]
+            elif xObject[obj]['/Filter'] == '/JPXDecode':
+                img = open(filename + ".jp2", "wb")
+                img.write(data)
+                img.close()
+                images+=[filename + ".jp2"]
+    return images
+
+def PullStructure(filename):
+    '''
+    Pulls the image of the molecular structure from page 2 as a png
+    '''
+    file = PyPDF2.PdfFileReader(open(filename, "rb"))
+    xObject = file.getPage(1)
+
+
+    xObject = xObject['/Resources']['/XObject'].getObject()
+
+    images=[]
+    
+    for obj in xObject:
+        if xObject[obj]['/Subtype'] == '/Image':
+            size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
+            data = xObject[obj].getData()
+            if xObject[obj]['/Filter'] == '/FlateDecode':
+                img = Image.frombytes("P", size, data)
+                img.save(filename + ".png")
+                images+=[filename + ".png"]
+    return images
+
+
+def PullText(filename):
+    '''
+    Pull text from the first page of a PDF
+
+    returns an array containing:
+    [ SpectrumID, CAS Number, Molecular Formula, Compound Name ]
+    '''
+    specID = ""
+    cas = ""
+    formula = ""
+    name = ""
 
     try:
-        abspath = path.abspath(filename)
-    except BaseException:
-        print('Usage :\nPDF_extract_images file.pdf page1 page2 page3 …')
-        sys.exit()
+        file = PyPDF2.PdfFileReader(open(filename, "rb"))
+        page = file.getPage(0)
 
+        page_content = page.extractText()
 
-    f = PyPDF2.PdfFileReader(open(filename, "rb"))
+        idIndex = page_content.find("Spectrum ID")
+        casIndex = page_content.find("CAS Registry Number")
+        formulaIndex = page_content.find("Formula")
+        nameIndex = page_content.find("CAS Index Name")
+        sourceIndex = page_content.find("Source")
+        startIndex = casIndex
 
-    page0 = f.getPage(0)
+        begin = idIndex + 11
+        end = casIndex
+        while begin != end:
+            specID += page_content[begin]
+            begin += 1
 
-    p = 1
-    images=recurse(p, page0)
+        begin = casIndex + 19
+        end = formulaIndex
+        while begin != end:
+            cas += page_content[begin]
+            begin += 1
 
-    return images
+        begin = formulaIndex + 7
+        end = nameIndex
+        while begin != end:
+            formula += page_content[begin]
+            begin += 1
+
+        begin = nameIndex + 14
+        end = sourceIndex 
+        while begin != end:
+            name += page_content[begin]
+            begin += 1
+    except: 
+        print("There was an error extracting text from the PDF")
+
+    return [specID, cas, formula, name]
